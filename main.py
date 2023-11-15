2bf5c6f371b3e32299aeb3c570ecaa784bd79371
vinicius <viniciusoliveira926@gmail.com>
2023-11-14 21:03:53 -0300

    Add quase finalizado

--- main.py
+++ main.py
@@ -1,43 +1,25 @@
 import re
-from analisado_lexico import tokenize_code
+
+from analisado_sintatico import analisar_sintaxe
+#from analisado_lexico import tokenize_code
 
 
 def prints():
     print("Test: 1")
     print("Test: 2")
     print("Test: 3")
-    print("Test: 4")
-    print("Test: 5")
-    print("Test: 6")
-    print("Test: 7")
-    print("Test: 8")
-    print("Test-Erro 1: 9")
-    print("Test-Erro 2: 10")
+
     
 
 def leitura_arquivos(code_test):
     
     match code_test:
         case 1:
-            caminho = './test_code/teste1.c'
+            caminho = './gramatica/ex1.pl0mod.txt'
         case 2: 
-           caminho = './test_code/teste2.c'
+           caminho = './gramatica/ex2.pl0mod.txt'
         case 3: 
-            caminho = './test_code/teste3.c'
-        case 4:
-            caminho = './test_code/teste4.c'
-        case 5:
-            caminho = './test_code/teste5.c'
-        case 6:
-            caminho = './test_code/teste6.c'
-        case 7:
-            caminho = './test_code/teste7.c'
-        case 8:
-            caminho = './test_code/teste8.c'
-        case 9:
-            caminho = './test_code/teste-erro1.c'
-        case 10:
-            caminho = './test_code/teste-erro2.c'
+            caminho = './gramatica/ex3.pl0mod.txt'
         case _:
             raise "Valor Nao Encontrado!"
 
@@ -45,9 +27,9 @@ def leitura_arquivos(code_test):
     with open(caminho, 'r', encoding='utf-8') as f:
         code = f.read()
         
-    cleaned_code = re.sub(r'/\*[\s\S]*?\*/|//.*', '', code, flags=re.DOTALL)
+    #cleaned_code = re.sub(r'/\*[\s\S]*?\*/|//.*', '', code, flags=re.DOTALL)
     
-    return cleaned_code
+    return code
    
 
 
@@ -56,12 +38,12 @@ def main():
     prints()
     num = int(input('Digite o numuro do arquivo: '))
     
-    content = leitura_arquivos(num)
+    code = leitura_arquivos(num)
     
-    values = tokenize_code(content)
 
-    for token in values:
-        print(token)
+    analisar_sintaxe(code)
+    
+    
     
 
 if __name__ == "__main__":
--- tokens/tokens.py
+++ tokens/tokens.py
@@ -18,3 +18,21 @@ class Token:
         
     def __str__(self) -> str:
         return f'Token Class: {self.token_class.name}, Value: {self.token_value}'
+
+#################################################################################################################################################################
+
+class TokenClassSintatico(Enum):
+    PALAVRA_RESERVADA = "(CONST|VAR|PROCEDURE|BEGIN|END|CALL|IF|THEN|WHILE|DO|PRINT|ODD|EVEN)"
+    OPERADOR = "(\+|-|\*|/|=|#|<|<=|>|>=|/\?)"
+    DELIMITADOR = "(,|;|\(|\))"
+    IDENT = "[a-zA-Z][a-zA-Z0-9]*"
+    NUMERO = "\d+"
+    ESPACO_EM_BRANCO = "\s+"
+
+class TokenSintatico:
+    def __init__(self, token_class: TokenClassSintatico, token_value) -> None:
+        self.token_class = token_class
+        self.token_value = token_value
+
+    def __str__(self) -> str:
+        return f'Classe do Token: {self.token_class.name}, Valor: {self.token_value}'
